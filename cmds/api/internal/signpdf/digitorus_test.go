3fb0ecd9571bd1655e9c3b41baa2de24d6ed8308
Alireza Ghasemi <agcombest@gmail.com>
2025-05-09 23:02:30 +0330

    Enable Digitorus PDF signer tests
    Now, after the bump update, they do pass.

--- cmds/api/internal/signpdf/digitorus_test.go
+++ cmds/api/internal/signpdf/digitorus_test.go
@@ -5,8 +5,7 @@ import (
 	"testing"
 )
 
-// Disabled, because it fails! The external library used in DigitorusPdfSigner is broken.
-func disabledTestDigitorusPdfSigner_Sign(t *testing.T) {
+func TestDigitorusPdfSigner_Sign(t *testing.T) {
 	ps := NewDigitorusPdfSigner(pvKey.(crypto.Signer), cert, certChains)
 	PdfSignerTests(t, ps)
 }
